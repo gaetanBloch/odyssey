0bd0a9cc2d09a425b5502b38ff01c2150c5af812
Gaetan Bloch <gaetan.bloch@gmail.com>
2021-07-09 22:10:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ const port = 3000
 app.use(express.static('public'));
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Application listening at http://localhost:${port}`)
 })
