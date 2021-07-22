0177d1506b616f78e6cbdce53b3ca54708396acc
lowzonenose <jpbazonnais@gmail.com>
2021-07-22 15:40:59 +0200

    Gestion async du rendu du vecteur tuilé

--- public/map.js
+++ public/map.js
@@ -4,25 +4,46 @@ const go = () => {
     const ignOLLayer = new ol.layer.VectorTile({
         title: "Plan IGN vecteur",
         visible: true,
+        opacity: 0.8,
         source: new ol.source.VectorTile({
             format: new ol.format.MVT(),
             url: `https://wxs.ign.fr/${ignKey}/geoportail/tms/1.0.0/PLAN.IGN/{z}/{x}/{y}.pbf`,
+            tileGrid : ol.tilegrid.createXYZ({
+                // extent : [minx, miny, maxx, maxy],
+                maxZoom : 22,
+                minZoom : 1,
+                tileSize : 256
+            }),
         }),
         attributions: '<a href="https://geoservices.ign.fr/documentation/geoservices/vecteur-tuile.html">&copy; IGN</a>',
         declutter: false
     });
 
+    
     const fetchStyle = async () => {
         let plan = await fetch(
             `ign/classique.json`
             // `https://wxs.ign.fr/${ignKey}/static/vectorTiles/styles/PLAN.IGN/classique.json`
         );
         const style = await plan.json();
-        olms.applyStyle(ignOLLayer, style, "plan_ign");
+        const setStyle = async () => {
+            olms.applyStyle(ignOLLayer, style, "plan_ign");
+        };
+        map.addLayer(ignOLLayer);
+        if (ignOLLayer.getSource()) {
+            await setStyle();
+        } else {
+            ignOLLayer.once("change:source", setStyle);
+        }
+        
     }
 
     const map = new ol.Map({
-        layers: [ignOLLayer],
+        layers: [
+            new ol.layer.GeoportalWMTS({
+                layer : "ORTHOIMAGERY.ORTHOPHOTOS"
+            })
+        ],
         target: document.getElementById('map'),
         view: new ol.View({
             center: [287963, 5948655],
@@ -30,7 +51,9 @@ const go = () => {
         })
     });
 
-    fetchStyle().catch(console.error);
+    fetchStyle()
+    .then()
+    .catch(console.error);
 }
 
 // Connection to Geoportail server
