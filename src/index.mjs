c345ac44f4db236867b6d366a1b17a2106330ded
gbloch <gaetan.bloch@gmail.com>
2021-08-30 10:44:04 +0200

    feat(cors): enable CORS for all origin on port 3000

--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 const port = 3000;
 
 // Set up the server
+// Enable CORS for port 3000
+app.use(cors());
 app.use(express.static('public'));
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
