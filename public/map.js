4f840accd8bd3fcfdd8bf8fe9d86c8e861b27deb
gbloch <gaetan.bloch@gmail.com>
2021-08-30 10:10:53 +0200

    feat(itinerary): support display of itinerary using IGN API

--- public/map.js
+++ public/map.js
@@ -1,6 +1,25 @@
-const go = () => {
-  const ignKey = 'choisirgeoportail';
+const ignKey = 'choisirgeoportail';
+const map = new ol.Map({
+  target: document.getElementById('map'),
+  view: new ol.View({
+    center: [287963, 5948655],
+    zoom: 6,
+    constrainResolution: true,
+    maxZoom: 21,
+    minZoom: 0,
+    resolutions: [
+      156543.033928041, 78271.51696402048, 39135.758482010235,
+      19567.87924100512, 9783.93962050256, 4891.96981025128, 2445.98490512564,
+      1222.99245256282, 611.49622628141, 305.7481131407048, 152.8740565703525,
+      76.43702828517624, 38.21851414258813, 19.10925707129406,
+      9.554628535647032, 4.777314267823516, 2.388657133911758,
+      1.194328566955879, 0.5971642834779395, 0.2985821417389697,
+      0.1492910708694849, 0.0746455354347424,
+    ],
+  }),
+});
 
+const go = () => {
   const ignOLLayer = new ol.layer.VectorTile({
     title: 'Plan IGN vecteur',
     visible: true,
@@ -20,10 +39,19 @@ const go = () => {
     declutter: true,
   });
 
-  const fetchStyle = async () => {
+  map.addLayer(
+    new ol.layer.GeoportalWMTS({
+      // layer: 'GEOGRAPHICALGRIDSYSTEMS.MAPS',
+      // layer: 'OI.OrthoimageCoverage',
+      layer: 'ORTHOIMAGERY.ORTHOPHOTOS',
+    })
+  );
+
+  // Fetch style IIFE
+  (async () => {
     let plan = await fetch(
       // `ign/standard.json`
-        `https://wxs.ign.fr/${ignKey}/static/vectorTiles/styles/PLAN.IGN/standard.json`
+      `https://wxs.ign.fr/${ignKey}/static/vectorTiles/styles/PLAN.IGN/standard.json`
     );
     const style = await plan.json();
     const setStyle = async () => {
@@ -35,41 +63,41 @@ const go = () => {
     } else {
       ignOLLayer.once('change:source', setStyle);
     }
-  };
-
-  const map = new ol.Map({
-    layers: [
-      new ol.layer.GeoportalWMTS({
-        // layer: 'GEOGRAPHICALGRIDSYSTEMS.MAPS',
-        // layer: 'OI.OrthoimageCoverage',
-        layer: 'ORTHOIMAGERY.ORTHOPHOTOS',
-      }),
-    ],
-    target: document.getElementById('map'),
-    view: new ol.View({
-      center: [287963, 5948655],
-      zoom: 6,
-      constrainResolution: true,
-      maxZoom: 21,
-      minZoom: 0,
-      resolutions: [
-        156543.033928041, 78271.51696402048, 39135.758482010235,
-        19567.87924100512, 9783.93962050256, 4891.96981025128, 2445.98490512564,
-        1222.99245256282, 611.49622628141, 305.7481131407048, 152.8740565703525,
-        76.43702828517624, 38.21851414258813, 19.10925707129406,
-        9.554628535647032, 4.777314267823516, 2.388657133911758,
-        1.194328566955879, 0.5971642834779395, 0.2985821417389697,
-        0.1492910708694849, 0.0746455354347424,
-      ],
-    }),
-  });
-
-  fetchStyle().then().catch(console.error);
+  })().catch(console.error);
 };
 
-// Connection to Geoportail server
+// Connection to Geoportal server
 Gp.Services.getConfig({
   serverUrl: './ign/autoconf-https.json',
   callbackSuffix: '',
   onSuccess: go,
+});
+
+//
+
+document.getElementById('find').addEventListener('click', async () => {
+  const origin = document.getElementById('origin').value;
+  const destination = document.getElementById('destination').value;
+  const url = `http://wxs.ign.fr/${ignKey}/itineraire/rest/route.json?origin=${origin}&destination=${destination}&method=DISTANCE&graphName=Pieton`;
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  const wkt = data.geometryWkt;
+  const format = new ol.format.WKT();
+  const feature = format.readFeature(wkt, {
+    dataProjection: 'EPSG:4326',
+    featureProjection: 'EPSG:3857',
+  });
+  const vector = new ol.layer.Vector({
+    source: new ol.source.Vector({
+      features: [feature],
+    }),
+    style: new ol.style.Style({
+      stroke: new ol.style.Stroke({
+        color: 'rgba(0,0,0,0.7)',
+        width: 3,
+      }),
+    }),
+  });
+  map.addLayer(vector);
 });
