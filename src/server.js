b226b4c1cbc0f255bcf3b573a3c804ddc6e884c1
gbloch <gaetan.bloch@gmail.com>
2021-09-03 10:59:27 +0200

    fix: use dynamic port for Heroku

--- src/server.js
+++ src/server.js
@@ -4,4 +4,4 @@ var app = express();
 console.log(__dirname + '/../dist');
 app.use(express.static(__dirname + '/../dist'));
 
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
