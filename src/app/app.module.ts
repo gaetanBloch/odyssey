ae9313f447518abf6b72e5d4f1f8f9f2b02390b0
gbloch <gaetan.bloch@gmail.com>
2021-09-08 07:55:52 +0200

    feat: added settings left menu with link to openlayers map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BodyModule } from './body/body.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, BodyModule ],
+  imports: [BrowserModule, AppRoutingModule, BodyModule, BrowserAnimationsModule ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -5,9 +5,10 @@ import { BodyRoutingModule } from './body-routing.module';
 import { MapLandingComponent } from './map-landing/map-landing.component';
 import { MapOlContainerModule } from './map-ol-container/map-ol-container.module';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
-  imports: [CommonModule, BodyRoutingModule, MapOlContainerModule],
+  imports: [CommonModule, MaterialModule, BodyRoutingModule, MapOlContainerModule],
   exports: [],
   declarations: [BodyComponent, MapSettingsComponent, MapLandingComponent],
   providers: [],
