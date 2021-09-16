33ad70d0c8e8069bd84d4df7cbf37cd4da75388c
gbloch <gaetan.bloch@gmail.com>
2021-09-16 08:35:27 +0200

    feat: Defined settings Type

---
+++ src/app/types/Settings.ts
@@ -0,0 +1,51 @@
+export interface Settings {
+  secrets: Map<string, string>,
+  maps: Maps,
+  features: Features,
+}
+
+interface Maps {
+  ol: {
+    ignVector: {
+      title: string,
+      default?: boolean,
+      type: string,
+      format: string,
+      tileUrl: string,
+      styleUrl: string
+      attributions: Array<string>
+    },
+    osmRaster: {
+      title: string,
+      default?: boolean
+    }
+  };
+  leaflet?: any;
+}
+
+interface Features {
+  ol: {
+    geolocation: Array<{
+      title: string,
+      default?: boolean,
+      method: string,
+      requestUrl: string,
+    }>,
+    reverseGeolocation: Array<{
+      title: string,
+      default?: boolean,
+      method: string,
+      requestUrl: string,
+    }>,
+    itinerary: Array<{
+      title: string,
+      default?: boolean,
+      method: string
+      requestUrl: string,
+      format: string,
+      drawField: string,
+      distanceField: string,
+      durationField: string,
+    }>
+  }
+}
